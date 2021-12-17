44b6c98d32707e8bbed12b12de271df09aa9ca18
andmohiko <andmohiko@gmail.com>
2021-12-18 06:17:11 +0900

    add favicons

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -4,7 +4,14 @@ import React from 'react'
 const HeadComponent: React.FC = () => (
   <Head>
     <title>eighteen</title>
-    <link rel="shortcut icon" href="/favicon.ico" />
+    <link rel="shortcut icon" href="/favicons/favicon.ico" />
+    <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+    <link rel="manifest" href="/favicons/site.webmanifest" />
+    <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#5bbad5" />
+    <meta name="msapplication-TileColor" content="#CBD5E0" />
+    <meta name="theme-color" content="#ffffff" />
     <meta name="Description" content="十八番アプリ eighteen" />
   </Head>
 )
