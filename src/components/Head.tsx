81373ea144cf03cd81149971e45982fe17df6743
andmohiko <andmohiko@gmail.com>
2021-12-19 07:36:54 +0900

    config safe area

--- src/components/Head.tsx
+++ src/components/Head.tsx
@@ -12,6 +12,9 @@ const HeadComponent: React.FC = () => (
     <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#5bbad5" />
     <meta name="msapplication-TileColor" content="#CBD5E0" />
     <meta name="theme-color" content="#ffffff" />
+    <meta name="viewport" content="initial-scale=1, viewport-fit=cover, width=device-width" />
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
     <meta name="Description" content="十八番アプリ eighteen" />
   </Head>
 )
