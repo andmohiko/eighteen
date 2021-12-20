ff9d419259f90da22c8b7e4ec5511c74a2c47fb5
andmohiko <andmohiko@gmail.com>
2021-12-21 08:36:16 +0900

    add mypage

---
+++ src/pages/mypage.tsx
@@ -0,0 +1,28 @@
+import Link from 'next/link'
+import { useRecoilValue } from 'recoil'
+import Layout from 'components/Layout'
+import { Box, Button, Text } from '@chakra-ui/react'
+import { userState } from 'atoms'
+import { useCheckLogin } from 'hooks/useCheckLogin'
+import { Logout } from 'lib/firebase'
+
+const MypagePage = () => {
+  const user = useRecoilValue(userState)
+  useCheckLogin()
+
+  return (
+    <Layout>
+      <Box justifyContent={"space-between"}>
+        <Box p={4}>
+          <Text fontSize="2xl">{user.username}</Text>
+          <Text fontSize="base" mt={2}>登録曲数: {user.repertory}</Text>
+        </Box>
+        <Box p={4} textAlign={"right"}>
+          <Button onClick={Logout}>ログアウト</Button>
+        </Box>
+      </Box>
+    </Layout>
+  )
+}
+
+export default MypagePage
