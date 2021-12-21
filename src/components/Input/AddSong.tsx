50518160fe1c35d40e9fd47210e33ce385216a8d
andmohiko <andmohiko@gmail.com>
2021-12-21 14:42:20 +0900

    fix div Box

--- src/components/Input/AddSong.tsx
+++ src/components/Input/AddSong.tsx
@@ -55,58 +55,56 @@ const AddSong: React.FC<Props> = ({ userId }) => {
   }
 
   return (
-    <>
+    <Box alignItems="center" px={2}>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <Box alignItems="center" px={2}>
         <Text fontSize="xl" textAlign="center" my={2}>持ち曲を追加する</Text>
-          <FormControl>
-            <Box alignItems="center" mb={4}>
-              <FormLabel w={100} htmlFor="title">
-                曲名
-              </FormLabel>
-              <Input
-                id="title"
-                placeholder="エンヴィキャットウォーク"
-                {...register('title')}
-              />
-            </Box>
-          </FormControl>
-          <FormControl>
-            <Box alignItems="center" mb={4}>
-              <FormLabel w={100} htmlFor="artist">
-                アーティスト
-              </FormLabel>
-              <Input
-                id="artist"
-                placeholder="トーマ"
-                {...register('artist')}
-              />
-            </Box>
-          </FormControl>
-          <FormControl>
-            <Box alignItems="center" mb={4}>
-              <FormLabel w={100} htmlFor="key">
-                キー
-              </FormLabel>
-              <Input
-                id="key"
-                placeholder="+3"
-                {...register('key')}
-              />
-            </Box>
-          </FormControl>
-          <Button
-            colorScheme="blue"
-            isLoading={isSubmitting}
-            type="submit"
-            display="flex"
-          >
-            <AddIcon mb="3px" />
-            <Text ml={2}>追加する</Text>
-          </Button>
-        </Box>
+        <FormControl>
+          <Box alignItems="center" mb={4}>
+            <FormLabel w={100} htmlFor="title">
+              曲名
+            </FormLabel>
+            <Input
+              id="title"
+              placeholder="エンヴィキャットウォーク"
+              {...register('title')}
+            />
+          </Box>
+        </FormControl>
+        <FormControl>
+          <Box alignItems="center" mb={4}>
+            <FormLabel w={100} htmlFor="artist">
+              アーティスト
+            </FormLabel>
+            <Input
+              id="artist"
+              placeholder="トーマ"
+              {...register('artist')}
+            />
+          </Box>
+        </FormControl>
+        <FormControl>
+          <Box alignItems="center" mb={4}>
+            <FormLabel w={100} htmlFor="key">
+              キー
+            </FormLabel>
+            <Input
+              id="key"
+              placeholder="+3"
+              {...register('key')}
+            />
+          </Box>
+        </FormControl>
+        <Button
+          colorScheme="blue"
+          isLoading={isSubmitting}
+          type="submit"
+          display="flex"
+        >
+          <AddIcon mb="3px" />
+          <Text ml={2}>追加する</Text>
+        </Button>
       </form>
-    </>
+    </Box>
   )
 }
 
